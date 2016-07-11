cafae47125b61636796a42267ebe6478a0a69b91
Snikz <your.snikz@icloud.com>
2016-07-11 23:30:03 +0300

    first commit

--- client/controllers.js
+++ client/controllers.js
@@ -57,3 +57,38 @@ angular.module('myApp').controller('registerController',
     };
 
 }]);
+angular.module('myApp').controller('logoutController',
+    ['$scope', '$location', 'AuthService',
+        function ($scope, $location, AuthService) {
+
+            $scope.logout = function(){
+                    AuthService.logout().then(function(){
+                        $location.path('/login')
+                    })
+            };
+    //
+    //$scope.register = function () {
+    //
+    //    // initial values
+    //    $scope.error = false;
+    //    $scope.disabled = true;
+    //
+    //    // call register from service
+    //    AuthService.register($scope.registerForm.username, $scope.registerForm.password)
+    //    // handle success
+    //        .then(function () {
+    //            $location.path('/login');
+    //            $scope.disabled = false;
+    //            $scope.registerForm = {};
+    //        })
+    //        // handle error
+    //        .catch(function () {
+    //            $scope.error = true;
+    //            $scope.errorMessage = "Something went wrong!";
+    //            $scope.disabled = false;
+    //            $scope.registerForm = {};
+    //        });
+    //
+    //};
+
+}]);
